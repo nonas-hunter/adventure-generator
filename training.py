b32a420e2d726264c0ef361447964177ef13570f
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2020-12-14 00:41:36 -0500

    Add docstrings to training.py

--- training.py
+++ training.py
@@ -39,7 +39,7 @@ class Training:
 
         Args:
             dataset: Filename of dataset (including .csv).
-            model_file: Name of file where parameters will be saved. 
+            model_file: Name of file where parameters will be saved.
         Assumptions:
             dataset files are located in the data folder.
             model files are located in the model folder.
@@ -113,7 +113,7 @@ class Training:
     @staticmethod
     def run_epoch(data_iter, model, loss_compute):
         """
-        Standard training and logging function.
+        Helper: Standard training and logging function.
 
         Args:
             data_iter: List of Batch instances containing data to be
@@ -146,7 +146,7 @@ class Training:
     @staticmethod
     def get_std_opt(model):
         """
-        Create a standard optimization function.
+        Helper: Create a standard optimization function.
 
         Args:
             model: Instance of Transformer class which will use the
@@ -164,7 +164,7 @@ class Training:
     @staticmethod
     def data_gen(V, batch, number_batches):
         """
-        Generate random data for a src-tgt copy task.
+        Helper: Generate random data for a src-tgt copy task.
 
         Args:
             V: Integer size of data to be generated.
@@ -190,6 +190,7 @@ class Batch:
         trg: List of target data.
         trg_mask: List representing how much of the target data to mask at any
             given time.
+        ntokens: Integer number of token words in batch.
     """
 
     def __init__(self, src, trg=None, pad=0):
@@ -213,7 +214,15 @@ class Batch:
     @staticmethod
     def make_std_mask(tgt, pad):
         """
-        Create a mask to hide padding and future words.
+        Helper: Create a mask to hide padding and future words.
+
+        Args:
+            tgt: List of tokens.
+            pad: Integer amount to describe how much data the list was padded
+                with.
+        Returns:
+            A list of booleans with the same length as the target list which
+            represents what words the model should address at any given moment.
         """
         tgt_mask = (tgt != pad).unsqueeze(-2)
         tgt_mask = tgt_mask & Variable(
@@ -222,17 +231,47 @@ class Batch:
 
     @staticmethod
     def rebatch(pad_idx, src_in, trg_in):
-        "Fix order in torchtext to match ours"
+        """
+        Helper: Transpose tensors from loaded datasets so they have the proper
+        dimensions.
+
+        Args:
+            pad_idx: Integer amount of data to pad original data with.
+            src_in: List of source data.
+            trg_in: List of target data.
+        """
         src, trg = src_in.transpose(0, 1), trg_in.transpose(0, 1)
         return Batch(src, trg, pad_idx)
 
 
 class NoamOpt:
     """
-    Optim wrapper that implements rate.
+    Optimizer function wrapper that implements rate.
+
+    Currently, this model uses the Adam Optimizer. This is subject to change
+    in the future.
+
+    Attributes:
+        optimizer: Optimizer function.
+        _step: Integer step size for each itteration.
+        warmup: Integer warmup step amount. This describes how many steps
+            before the rate switches from linearly increasing to increasing
+            proportionally with the inverse square root.
+        factor: Float factor to multiply with output of function.
+        model_size: Integer dimension of model input layer.
+        _rate: Rate at which optimizer function should change. Rate changes
+            as _step increases.
     """
 
     def __init__(self, model_size, factor, warmup, optimizer):
+        """
+        Instantiates an optimizer wrapper.
+
+        Args:
+            model_size: Integer dimension of model input layer.
+            factor: Float factor to multiply with outut of optimizer function.
+            warmup: Integer of steps to be considered apart of the warmup.
+        """
         self.optimizer = optimizer
         self._step = 0
         self.warmup = warmup
@@ -254,6 +293,12 @@ class NoamOpt:
     def rate(self, step=None):
         """
         Implement 'lrate' above
+
+        Args:
+            step: Integer step amount used to override saved step amount.
+        Returns:
+            The rate to be used with the optimizer function based on current
+            step.
         """
         if step is None:
             step = self._step
@@ -264,20 +309,45 @@ class NoamOpt:
 
 class LabelSmoothing(nn.Module):
     """
-    Implement label smoothing.
+    Implement label smoothing to reduce model confidence while training.
+
+    Attributes:
+        criterion: Function which decides if a node is producing the correct
+            response.
+        padding_idx: Integer representation of <PAD> token.
+        confidence: Float representing the models confidence in its answers.
+        smoothing: Float representing amount of smoothing applied.
+        size: Integer size of the model.
+        true_dist: The distance between the correct answer and the given data.
     """
 
     def __init__(self, size, padding_idx, smoothing=0.0):
+        """
+        Instantiate a LabelSmoothing object.
+
+        Args:
+            size: Integer size of the model.
+            padding_idx: Integer representation of <PAD> token.
+            smoothing: Float representing amount of smoothing applied.
+        """
         super(LabelSmoothing, self).__init__()
         self.criterion = nn.KLDivLoss(size_average=False)
         self.padding_idx = padding_idx
-        self.confidence = 1.0
+        self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
         self.size = size
         self.true_dist = None
 
     def forward(self, x, target):
-        print(x)
+        """
+        Feed data forward through LabelSmoothing layer.
+
+        For use with the pytorch library.
+
+        Args:
+            x: Input data.
+            target: Expected output data.
+        """
         assert x.size(1) == self.size
         true_dist = x.data.clone()
         true_dist.fill_(self.smoothing / (self.size - 2))
@@ -293,14 +363,35 @@ class LabelSmoothing(nn.Module):
 class SimpleLossCompute:
     """
     A simple loss compute and train function.
+
+    Attributes:
+        generator: Instance of the generator class defined in the model.
+        criterion: Instance of LabelSmoothing class.
+        opt: Instance of NoamOpt class.
     """
 
     def __init__(self, generator, criterion, opt=None):
+        """
+        Create instance of SimpleLossCompute class.
+
+        Args:
+            generator: Instance of the generator class defined in the model.
+            criterion: Instance of LabelSmoothing class.
+            opt: Instance of NoamOpt class.
+        """
         self.generator = generator
         self.criterion = criterion
         self.opt = opt
 
     def __call__(self, x, y, norm):
+        """
+        Calculates the loss (or incorrectness) of data.
+
+        Args:
+            x: Source data.
+            y: Target data.
+            norm: Normalization factor
+        """
         x = self.generator(x)
         loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
                               y.contiguous().view(-1)) / norm
@@ -315,19 +406,49 @@ class SimpleLossCompute:
 class Vocabulary:
     """
     Class containing the models vocabulary based on given dataset.
+
+    Attributes:
+        itos: Dictionary which translates indecies to tokens
+        stoi: Dictionary which translates tokens to indecies
+        freq_threshold: Integer representing amount of times a word needs to
+            appear in the dataset for it to become a token.
+        spacey_en: Sentence tokenizer defined in spacy package.
     """
 
     spacy_en = spacy.load('en')
 
     def __init__(self, freq_threshold):
+        """
+        Instantiate a vocabulary object.
+
+        Args:
+            freq_threshold: Integer representing amount of times a word needs
+                to appear in the dataset for it to become a token.
+        """
         self.itos = {0: "<PAD>", 1: "<SOS>", 2: "<EOS>", 3: "<UNK>"}
         self.stoi = {"<PAD>": 0, "<SOS>": 1, "<EOS>": 2, "<UNK>": 3}
         self.freq_threshold = freq_threshold
 
     def __len__(self):
+        """
+        Returns the length of tokens/indecies in the vocabulary.
+        """
         return len(self.itos)
 
     def build_vocabulary(self, sentence_list):
+        """
+        Defines the itos and stoi based on a list of given sentences.
+
+        NOTE: This function may be replaced by a different function
+        which defines the vocbulary by using word grouping on a more
+        general set of vocabulary words. This will help increase the
+        model's accurary.
+
+        Args:
+            sentence_list: List of strings containing words to load into the
+                model. These sentences usually come from the dataset the model
+                was trained on.
+        """
         frequenecies = {}
         idx = 4
 
@@ -344,6 +465,17 @@ class Vocabulary:
                     idx += 1
 
     def numericalize(self, text, tokenize=True):
+        """
+        Convert a list of tokens (or a string) into a list of their respective
+        indecies.
+
+        Args:
+            text: A list of tokens or a string.
+            tokenize: A boolean determining if the text parameter needs to be
+                tokenized.
+        Returns:
+            A list of indicies representing the given text.
+        """
         if tokenize:
             text = self.tokenize(text)
 
@@ -368,25 +500,61 @@ class AdventureDataset(Dataset):
     """
     Represents a dataset of text used to train the model and translate strings
     into numerical representations.
+
+    Attributes:
+        root_dir: String containing the root directory of the project.
+        data_frame: Pandas data frame containg dataset read from a csv.
+        transform: Function indicating how data from dataframe needs to be
+            transformed.
+        src: Source data.
+        tgt: Target data.
+        vocab: Vocabulary object containing all vocabulary from the dataset
+            that appears atleast as many times of the given frequency
+            threshold.
     """
 
     def __init__(self, data_file, root_dir="./",
                  transform=None, freq_threshold=1):
+        """
+        Instantiate AdventureDataset object.
+
+        Args:
+            data_file: String containing path to csv file containing training
+                data.
+            root_dir: String containing root directory of project.
+            transform: Function indicating how data from dataframe needs to be
+                transformed.
+            freq_threshold: Integer number of times a word needs to appear in
+                the dataset to be converted into a token.
+        """
         self.root_dir = root_dir
-        self.df = pd.read_csv(data_file)
+        self.data_frame = pd.read_csv(data_file)
         self.transform = transform
 
         # Get source, target
-        self.src = self.df["source"]
-        self.tgt = self.df["target"]
+        self.src = self.data_frame["source"]
+        self.tgt = self.data_frame["target"]
         # Initialize vocabulary and build vocab
         self.vocab = Vocabulary(freq_threshold)
         self.vocab.build_vocabulary(self.src.tolist() + self.tgt.tolist())
 
     def __len__(self):
-        return len(self.df)
+        """
+        Return length of training data.
+        """
+        return len(self.data_frame)
 
     def __getitem__(self, index):
+        """
+        Return the source-target vectors for the given training data at a
+            specified index.
+
+        Args:
+            index: Integer index of the dataset.
+        Returns:
+            Numericalized and tokenized source-target data pair from the
+            dataset.
+        """
         source = self.src[index]
         target = self.tgt[index]
 
@@ -409,12 +577,30 @@ class AdventureDataset(Dataset):
 class AdventureCollate:
     """
     Restructures batches so that all batches are the same length.
+
+    Attributes:
+        pad_idx: Integer index representing the <PAD> token.
     """
 
     def __init__(self, pad_idx):
+        """
+        Instantiates AdventureCollate class and sets the index of the <PAD>
+        token.
+
+        Args:
+            pad_idx: Integer index representing the <PAD> token.
+        """
         self.pad_idx = pad_idx
 
     def __call__(self, batch):
+        """
+        Restructures batches so they are all roughly the same size.
+
+        Args:
+            batch: A collection of source and target data.
+        Returns:
+            The restructured source and target data.
+        """
         source = [item[0] for item in batch]
         source = pad_sequence(source,
                               batch_first=False,
